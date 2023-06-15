f7547d29ac410a88bd4467d6d21a7128dd735961
Isaac Frei <isaacfrei@mailfence.com>
2023-06-14 22:21:24 -0700

    add installation of custom builds to post-install.sh

--- post-install.sh
+++ post-install.sh
@@ -4,15 +4,22 @@ systemctl enable --now \
 	sshd.service \
 	cups.socket
 
-echo -e "\nCloning dotfiles..."
-mkdir $HOME/dotfiles
-git init --bare $HOME/dotfiles
-git --git-dir=$HOME/dotfiles --work-tree=$HOME branch -M main
-git --git-dir=$HOME/dotfiles --work-tree=$HOME config --local status.showUntrackedFiles no
-rm -rf .config $HOME/.*
-git --git-dir=$HOME/dotfiles --work-tree=$HOME pull --set-upstream https://github.com/isaiah7477/dotfiles
-
 echo -e "\nInstalling aur packages..."
-git clone https://aur.archlinux.org/paru.git /tmp/paru
+mkdir $HOME/.builds
+git clone https://aur.archlinux.org/paru.git $HOME/.builds/paru
 cd /tmp/paru && makepkg -si --noconfirm
 paru -S --needed --noconfirm - < pkglist.aur
+
+echo -e "\nInstalling custom builds..."
+git clone https://github.com/isaiah7477/dwm.git $HOME/.builds/dwm
+cd $HOME/.builds/dwm && sudo make clean install
+rm -rf $HOME/.dotfiles $HOME/.dotfiles.old
+
+echo -e "\nCloning dotfiles..."
+git clone --bare https://github.com/isaiah7477/dotfiles.git $HOME/.dotfiles
+function config {
+   /usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME $@
+}
+config reset --hard
+config checkout
+config config status.showUntrackedFiles no
