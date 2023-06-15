e611923042b60659b06069debb0b9de7cedd8ebe
Isaac Frei <isaacfrei@mailfence.com>
2023-06-14 21:12:06 -0700

    add script to clone dotfiles

--- clone-dotfiles.sh
+++ clone-dotfiles.sh
@@ -1,14 +1,8 @@
+rm -rf $HOME/.dotfiles $HOME/.dotfiles.old
 git clone --bare https://github.com/isaiah7477/dotfiles.git $HOME/.dotfiles
 function config {
    /usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME $@
 }
-mkdir -p .dotfiles.old
-config checkout
-if [ $? = 0 ]; then
-  echo "Checked out config.";
-  else
-    echo "Backing up pre-existing dot files.";
-    config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .dotfiles.old/{}
-fi;
+config reset --hard
 config checkout
 config config status.showUntrackedFiles no
